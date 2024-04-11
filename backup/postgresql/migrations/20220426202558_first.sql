c5e1b8b6b7d082f78861166739295367fe7596a3
Anastasiya Duplishcheva <aduplishcheva@ozon.ru>
2024-04-11 18:37:24 +0300

    [TATA-2986] feat: remove backup

--- backup/postgresql/migrations/20220426202558_first.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-select * from a;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-SELECT 'down SQL query';
--- +goose StatementEnd
--- backup/workshop-3/internal/domain/user.go
+++
@@ -1,6 +0,0 @@
-package domain
-
-type User struct {
-	UserID          int64
-	DefaultCurrency string
-}
